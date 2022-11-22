ceed96430dc34e6a31fda1494368355e6c6bf324
taekyu32-lee <taekyu32@gmail.com>
2022-11-22 16:40:23 +0900

    Feat: article model 생성 #2
    - Draft Model 생성
    - Size Model 생성

--- article/models.py
+++ article/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Draft(models.Model):
+    style = models.CharField(max_length=50)
+    image = models.TextField()
+
+class Size(models.Model):
+    draft = models.ForeignKey(Draft,on_delete=models.CASCADE, related_name='draft_set')
+    size = models.CharField(max_length=50)
+    stock = models.IntegerField(default=999)
+    price = models.IntegerField()
+    color = models.CharField(max_length=50)
+
