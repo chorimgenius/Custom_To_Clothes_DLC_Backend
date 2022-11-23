4bf0592d15b01fa905849416cf724593df0bc64e
seminju0172@gmail.com <seminju0172@gmail.com>
2022-11-23 17:53:18 +0900

    Feat: 메인페이지 기능 #9
    - serializers.py 작성
    - views.py 작성
    
    Close #9

--- article/views.py
+++ article/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from article.serializers import ArticleSerializer
+from article.models import Article
+from django.db.models import Count
 
 # Create your views here.
+class RankArticleView(APIView):
+    def get(self, request):
+        article = Article.objects.annotate(like_count = Count('likes')).order_by('-like_count')
+        print(article)
+        serializer = ArticleSerializer(article, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
