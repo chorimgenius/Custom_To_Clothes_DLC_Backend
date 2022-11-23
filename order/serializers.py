678eaa62ade6df18111858e623ce1474a49724fc
jinyeobjeong <yeobdol@gmail.com>
2022-11-23 09:40:51 +0900

    Feat: Clothes profile 기능구현  #6
    - Clothes profile get
    - article DB 에서 필요한 주문정보를 가져온다.

---
+++ order/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from order.models import Order
+from article.models import Article
+
+class OrderViewSerializer(serializers.ModelSerializer):
+    article_user = serializers.SerializerMethodField()
+    
+    def get_article_user(self, obj):
+        return obj.user.username
+    
+    class Meta:
+        model = Article
+        fields = ('id', 'article_user', 'image', 'user', 'draft',)
+        
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ('mount', 'status',)
