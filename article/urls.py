10c5528a17b16ee68dec2028fe077e544862fc5f
taekyu32-lee <taekyu32@gmail.com>
2022-11-23 21:38:12 +0900

    Feat: Article post, put 작업  #4
    - Article view - post Response 수정
    - Article view - put 작업
    
    Close #4

--- article/urls.py
+++ article/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from article import views
 
 urlpatterns = [
-    path('article/', views.CustomView.as_view())
+    path('article/', views.CustomView.as_view()),
     path('', views.RankArticleView.as_view(), name='order_view'),
 ]
